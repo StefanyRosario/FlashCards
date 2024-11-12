ede59f7f01271d9b46b97765b08e313c082bfb3a
StefanyRosario <108366312+StefanyRosario@users.noreply.github.com>
2024-11-12 10:40:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é algoritmo?',
+    'é uma sequência de passo ou instruçoẽs'
+)
+
+criaCartao(
+    'Programação',
+    'O que élogica da programação?',
+    ' é um conjunto de principios e tecnica de desenvolver.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'Linguagem para estilizar documentos HTML.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é email?',
+    'é uma forma de comunicação digital que permite receber mensagens'
+)
+
+criaCartao(
+    'Programação',
+    'o que é programação?',
+    'é o processo de criar e escrever instruçoẽs para que umcomputador execute determinadas tarefas'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um destrutor?',
+    'é uma função especial em programação orientada a objeto que é chamada automaticamente quando um objeto está sendo destruido ou removido da memória.'
+)
